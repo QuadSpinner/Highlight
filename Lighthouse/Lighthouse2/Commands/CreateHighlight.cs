8a059a26894c099a0ddba10e174c4d57272759a0
Dax Pandhi <dax@quadspinner.com>
2022-02-06 17:17:22 +0530

    Fresh start

---
+++ Lighthouse/Lighthouse2/Commands/CreateHighlight.cs
@@ -0,0 +1,11 @@
+﻿namespace Lighthouse2
+{
+    [Command(PackageIds.CreateHighlight)]
+    internal sealed class CreateHighlight : BaseCommand<CreateHighlight>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            await VS.MessageBox.ShowWarningAsync("Lighthouse2", "Button clicked");
+        }
+    }
+}
